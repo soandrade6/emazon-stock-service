c0cd513563ee880709be219e0bc4c018afa39092
soandrade6 <sofia.andradep06@gmail.com>
2024-08-21 22:18:52 -0500

    dto of request and response

---
+++ src/main/java/com/emazon/stock_service/application/dto/CategoryResponseDto.java
@@ -0,0 +1,11 @@
+package com.emazon.stock_service.application.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CategoryResponseDto {
+    private String name;
+    private String description;
+}
