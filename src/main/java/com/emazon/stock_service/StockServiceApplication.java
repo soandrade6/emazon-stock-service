a49ed27de83fdd146d485d2873d036627ede50ef
soandrade6 <sofia.andradep06@gmail.com>
2024-08-21 21:40:50 -0500

    initial commit

---
+++ src/main/java/com/emazon/stock_service/StockServiceApplication.java
@@ -0,0 +1,13 @@
+package com.emazon.stock_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emazon/stock_service/StockServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emazon.stock_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
