771f0f9b1a0f146910ed9d48ed69cd4ead2d8f56
soandrade6 <sofia.andradep06@gmail.com>
2024-08-22 12:06:09 -0500

    ADD: OpenApi documentation

--- src/main/java/com/emazon/stock_service/infraestruture/input/rest/CategoryRestController.java
+++ src/main/java/com/emazon/stock_service/infraestruture/input/rest/CategoryRestController.java
@@ -1,7 +1,14 @@
 package com.emazon.stock_service.infraestruture.input.rest;
 
 import com.emazon.stock_service.application.dto.CategoryRequestDto;
+import com.emazon.stock_service.application.dto.CategoryResponseDto;
 import com.emazon.stock_service.application.handler.ICategoryHandler;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,8 +24,23 @@ public class CategoryRestController {
 
     private final ICategoryHandler categoryHandler;
 
+    @Operation(
+            summary = "Create a new category",
+            description = "Endpoint to create a new category in the system.",
+            tags = { "Category" }
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Category created successfully",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = CategoryResponseDto.class))),
+            @ApiResponse(responseCode = "400", description = "Invalid input",
+                    content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal server error",
+                    content = @Content)
+    })
     @PostMapping
-    public ResponseEntity<Void> saveCategory(@RequestBody CategoryRequestDto categoryRequestDto){
+    public ResponseEntity<Void> saveCategory(@RequestBody @Parameter(description = "Category request data", required = true)
+                                                 CategoryRequestDto categoryRequestDto){
         categoryHandler.saveCategory(categoryRequestDto);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
