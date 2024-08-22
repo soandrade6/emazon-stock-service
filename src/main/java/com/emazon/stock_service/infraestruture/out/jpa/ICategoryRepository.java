9b6a0316a4037f14edb7975950a1ed57bb75439a
soandrade6 <sofia.andradep06@gmail.com>
2024-08-22 10:00:01 -0500

    category jpa adapter

--- src/main/java/com/emazon/stock_service/infraestruture/out/jpa/ICategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.emazon.stock_service.infraestruture.out.jpa;
-
-import com.emazon.stock_service.infraestruture.out.jpa.entity.CategoryEntity;
-
-import java.util.Optional;
-
-public interface ICategoryRepository {
-    Optional<CategoryEntity> findByName(String categoryName);
-}
---
+++ src/main/java/com/emazon/stock_service/infraestruture/out/jpa/adapter/CategoryJpaAdapter.java
@@ -0,0 +1,23 @@
+package com.emazon.stock_service.infraestruture.out.jpa.adapter;
+
+import com.emazon.stock_service.domain.model.Category;
+import com.emazon.stock_service.domain.spi.ICategoryPersistencePort;
+import com.emazon.stock_service.infraestruture.out.jpa.mapper.ICategoryEntityMapper;
+import com.emazon.stock_service.infraestruture.out.jpa.repository.ICategoryRepository;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class CategoryJpaAdapter implements ICategoryPersistencePort {
+
+    private final ICategoryRepository categoryRepository;
+    private final ICategoryEntityMapper categoryEntityMapper;
+
+    @Override
+    public void saveCategory(Category category) {
+        if (categoryRepository.findByName(category.getName()).isPresent()){
+
+        }
+
+        categoryRepository.save(categoryEntityMapper.toEntity(category));
+    }
+}
