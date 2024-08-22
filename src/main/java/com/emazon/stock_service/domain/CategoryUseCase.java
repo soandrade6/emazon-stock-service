1074d18313a59ff60d207e9f2dbe616f6fe5a5ee
soandrade6 <sofia.andradep06@gmail.com>
2024-08-21 22:12:18 -0500

    Name and description length exceptions

--- src/main/java/com/emazon/stock_service/domain/CategoryUseCase.java
+++ src/main/java/com/emazon/stock_service/domain/CategoryUseCase.java
@@ -1,6 +1,8 @@
 package com.emazon.stock_service.domain;
 
 import com.emazon.stock_service.domain.api.ICategoryServicePort;
+import com.emazon.stock_service.domain.exception.DescriptionTooLongException;
+import com.emazon.stock_service.domain.exception.NameTooLongException;
 import com.emazon.stock_service.domain.model.Category;
 import com.emazon.stock_service.domain.spi.ICategoryPersistencePort;
 
@@ -14,6 +16,11 @@ public class CategoryUseCase implements ICategoryServicePort {
 
     @Override
     public void save(Category category) {
+        if(category.getName().length() > 50){
+            throw new NameTooLongException();
+        }else if (category.getDescription().length() > 90){
+            throw new DescriptionTooLongException();
+        }
         categoryPersistencePort.save(category);
 
     }
